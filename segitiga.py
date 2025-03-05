56c3f3d9bae601ad46e8df6f9fa71964c4a5c1c0
ikanmakarel01 <moskovkokop@gmail.com>
2025-03-05 09:05:09 +0700

    Update segitiga.py

--- segitiga.py
+++ segitiga.py
@@ -4,3 +4,26 @@ for i in range(1, tinggi + 1):
     spasi = ' ' * (tinggi - i)
     bintang = '#' * (2*i - 1)
     print(spasi + bintang)
+
+
+tinggi = int(input("Masukkan tinggi segitiga: "))
+
+for i in range(1, tinggi + 1):
+    spasi = ' ' * (tinggi - i)  # Menghitung spasi di sebelah kiri
+    pagar = '#' * i              # Menghitung jumlah #
+    print(spasi + pagar)
+
+
+tinggi = int(input("Masukkan tinggi segitiga: "))
+
+for i in range(tinggi, 0, -1):
+    spasi = ' ' * (tinggi - i)  # Spasi bertambah setiap baris
+    pagar = '#' * i             # Jumlah # berkurang
+    print(spasi + pagar)
+
+tinggi = int(input("Masukkan tinggi segitiga: "))
+
+for i in range(tinggi, 0, -1):
+    spasi = ' ' * (tinggi - i)       # Spasi di sebelah kiri
+    pagar = '#' * (2 * i - 1)        # Jumlah # ganjil
+    print(spasi + pagar)
